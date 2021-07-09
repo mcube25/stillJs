b4e5acb1ac3e37105a2078097da7b3a3e1d04b12
mcube25 <mcube3567@gmail.com>
2021-07-09 14:50:51 -0700

    functions

--- sample.js
+++ sample.js
@@ -116,14 +116,158 @@ for (var i = 0; i <= person.length; i++) {
 var number = 5;
 while (number < 20) {
     console.log(number);
-    number +=7;
+    number += 7;
 }
 // do while loop
 var i = 6;
 do {
     i++
-} while (i<=9);
+} while (i <= 9);
 console.log(i);
+//do while allows you to perform a statement at least once 
+//whereas in while statement if it a false it never runs
+
+//break and continue keywords which can only be used within loops
+
+var number = 0;
+while (number < 5) {
+    console.log(number);
+    if (number == 2) {
+        break;// u can use continue here
+    }
+    number++;
+}
+//if statements allows us execute block of statements if condition is true
+var bool = true;
+if (bool) {
+    console.log("hello");
+} else {
+    console.log("hi");
+}
+//another example
+var self = {
+    age: 79,
+    clob: "SE3"
+}
+if (self.age > 23) {
+    console.log(self.clob);
+} else if (self.age >= 87) {
+    console.log('man u old');
+}
+else {
+    console.log('nothing is impossible');
+}
+//switch statements: when we have more than one case of if else
+
+switch (self.age) {
+    case (person.age == 45):
+        console.log('you are an adault');
+        break;
+
+    case (person.age == 85):
+        console.log('you old man');
+        break;
+
+    case (person.age == 234):
+        console.log('you are out');
+        break;
+    default:
+        console.log('we dont know');
+}
+//another example
+switch (new Date().getDay) {
+    case 6:
+        console.log('saturday of the week');
+        break;
+
+    default:
+        console.log('i dont know');
+}
+//comparison and logical operators 
+console.log(20 == 20);
+console.log(20 < 20);
+console.log(20 <= 20);
+console.log(20 > 20);
+console.log(20 >= 20);
+
+//logical operators
+console.log(20 == 20 && 30 == 30);
+//logical && means everything has to be true
+console.log(20 == 20 || 30 == 30);
+// logical or || means one statement has to be true
+//we use the comparison and logical operators in if statements
+
+//type coercion is the process of converting a value from one type to anoher
+console.log(typeof 0 + " " + typeof false);
+console.log(0 == false); //false is being converted to zero
+
+console.log(typeof "0" + " " + typeof false);
+console.log("0" == false);
+
+console.log(typeof 2 + " " + typeof "2");
+console.log("2" == 2);
+
+// use the strict equality === to prevent type coercion
+console.log(typeof 0 + " " + typeof false);
+console.log(0 === false); //false is being converted to zero
+
+console.log(typeof "0" + " " + typeof false);
+console.log("0" === false);
+
+console.log(typeof 2 + " " + typeof "2");
+console.log("2" === 2);
+
+//map, filter and reduce
+//map when working with arrays
+//map simply means transform which means the function 
+//in the map will be executed for every single element in an array
+var mep = [2, 3, 4, 5, 6, 7].map((n) => {
+    return n * 4;
+});
+console.log(mep);
+
+//filter simply allows us to filter values of an array
+//returns a boolean
+var mip = [2, 3, 4, 5, 6, 7].filter((n) => {
+    return n % 2 == 0;
+});
+console.log(mip);
+
+//reduce allows the reduction of all the values inside of an array
+//into one single value
+var myp = [2, 3, 4, 5, 6, 7].reduce((accumulator, current) => {
+    return accumulator + current
+})
+console.log(myp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
